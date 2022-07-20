a08d5aea230a665d9f998b5d1593ca2540b45ccf
stanleyphu <phu.stanley@gmail.com>
2022-07-20 15:13:57 -0700

    Export WarrantCheck and Warrant types

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as Client } from "./Client";
 export { WARRANT_IGNORE_ID } from "./constants";
+export { default as Warrant } from "./types/Warrant";
+export { default as WarrantCheck } from "./types/WarrantCheck";
