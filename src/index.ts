25e227f07adade8156b70973e9e62d069854ad06
stanleyphu <phu.stanley@gmail.com>
2022-09-26 14:12:44 -0700

    Export WarrantCheckOp

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
+export * from "./types";
 export { default as Client } from "./Client";
-export { default as Warrant } from "./types/Warrant";
-export { default as WarrantCheck } from "./types/WarrantCheck";
---
+++ src/types/index.ts
@@ -0,0 +1,2 @@
+export { default as Warrant } from "./Warrant";
+export { default as WarrantCheck, WarrantCheckOp } from "./WarrantCheck";
